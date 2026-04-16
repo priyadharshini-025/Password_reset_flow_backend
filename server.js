9cbde6e6dc00762932819d154a5f53db40ea2779
priyadharshini-025 <priyachandrasekar2002@gmail.com>
2026-04-16 12:22:19 +0530

    added

--- server.js
+++ server.js
@@ -21,6 +21,18 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/auth", (req, res) => {
+  res.send("API is running...");
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 
